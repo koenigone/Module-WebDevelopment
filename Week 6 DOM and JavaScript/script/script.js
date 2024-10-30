da7b236e1b35ce19aedfdf9a14e79423719e1587
Mohammad  Mohammad <mm6dd.mh@gmail.com>
2024-10-30 15:28:49 +0000

    completed week 6 exercise

---
+++ Week 6 DOM and JavaScript/script/script.js
@@ -0,0 +1,30 @@
+let mainBtn = document.getElementById("myTestBtn");
+let red    = "red";
+let green  = "green";
+let blue   = "blue";
+let normal = "white";
+let coloursArr = [red, green, blue, normal];
+let value  = 0;
+
+mainBtn.addEventListener("click", ()=> {
+  document.body.style.backgroundColor = coloursArr[value];
+  value = (value + 1) % coloursArr.length;
+});
+
+let redBtn = document.querySelector(".red");
+let greenBtn = document.querySelector(".green");
+let blueBtn = document.querySelector(".blue");
+let resetBtn = document.querySelector(".reset");
+
+redBtn.addEventListener("click", ()=> {
+    document.body.setAttribute("class", "red");
+});
+greenBtn.addEventListener("click", ()=> {
+    document.body.setAttribute("class", "green");
+});
+blueBtn.addEventListener("click", ()=> {
+    document.body.setAttribute("class", "blue");
+});
+resetBtn.addEventListener("click", ()=> {
+    document.body.setAttribute("class", "reset");
+});
