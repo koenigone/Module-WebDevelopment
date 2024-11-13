a10a8d4d48781b937948fc30f3026c258c6a5c1a
Mohammad  Mohammad <mm6dd.mh@gmail.com>
2024-11-13 15:50:07 +0000

    added week 8 lab work - not completed

---
+++ Week 8 json data/readme.md
@@ -0,0 +1,5 @@
+# Working with Javascript Objects - Starter Repo
+
+This Repo is designed as a teaching aid for those interesting in learning about Javascript Objects.
+
+The instructions for this tutorial are in the [Repo Wiki](https://github.com/mustbebuilt/javascript-objects-json-lab/wiki).
---
+++ Week 8 json data/script/staff.js
@@ -0,0 +1,19 @@
+fetch("data/staff.json")
+    .then(response => response.json)
+    .then(data => {
+        Array.from(data).forEach(item => {
+            let staffTable = document.getElementById("staffTable");
+            const row = document.createElement("tr");
+            const nameCell = document.createElement("td");
+            const emailCell = document.createElement("td");
+
+            nameCell.textContent = `${item.first_name} ${item.last_name}`;
+            emailCell.textContent = item.email;
+            row.appendChild(nameCell);
+            row.appendChild(emailCell);                
+            staffTable.appendChild(row);
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    })
